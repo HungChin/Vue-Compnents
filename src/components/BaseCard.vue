fcf3059479c426712fe3fb27d10dab4781c62b4f
HungChin <HungChin02244@gmail.com>
2025-06-20 17:41:50 +0800

    加入外部元件練習2

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card" style="width: 18rem;">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+      <h5 class="card-title">{{ title }}</h5>
       <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s
         content.</p>
@@ -10,3 +10,13 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: '卡片練習標題'
+    }
+  }
+}
+</script>
