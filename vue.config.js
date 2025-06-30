86ce54d2a25d06d55fd6aba6f6e288a0765774e9
Chin <HungChin02244@gmail.com>
2025-06-30 22:07:28 +0800

    環境變數設定及編譯設定練習20250630

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-
-  transpileDependencies: false
+  transpileDependencies: false,
+  publicPath: '/dist/'
 })
 // module.exports = {
 //   lintOnSave: false
